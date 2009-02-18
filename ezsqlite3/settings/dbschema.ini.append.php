bb7741a3f6a734865389239052572ed0bce48237
Kristof Coomans <kristof.coomans@telenet.be>
2009-02-18 13:19:53 +0000

    - Workaround for: eZDBSchema::instance() uses $db->databaseName() (sqlite) if no type is passed through $params, and schema handler (sqlite3) can not be found

--- ezsqlite3/settings/dbschema.ini.append.php
+++ ezsqlite3/settings/dbschema.ini.append.php
@@ -4,4 +4,7 @@
 SchemaPaths[sqlite3]=extension/ezsqlite3/ezdb/dbms-schema/ezsqliteschema.php
 SchemaHandlerClasses[sqlite3]=eZSQLiteSchema
 
+SchemaPaths[sqlite]=extension/ezsqlite3/ezdb/dbms-schema/ezsqliteschema.php
+SchemaHandlerClasses[sqlite]=eZSQLiteSchema
+
 */ ?>
